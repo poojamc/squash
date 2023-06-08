1d22ea78c52fd371ded952d6dfc8bc7f8cc42523
poojamc <134255557+poojamc@users.noreply.github.com>
2023-06-08 17:05:11 +0530

    Create disk.sh

---
+++ disk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+size=`df -h .| tail -1| awk -F " " '{print $(NF -1)}'| sed 's/%//Ig'`
+body=echo -e "The disc memory is full"
+if [ $size -gt 20 ] 
+then
+echo $body | mail -s "Memory Reached 20% " mcpooja29114@gmail.com
+fi
