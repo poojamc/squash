e6af680ebaf0a741f325e39cc580d038d315974e
poojamc <134255557+poojamc@users.noreply.github.com>
2023-06-08 17:00:15 +0530

    Update serv.sh

--- serv.sh
+++ serv.sh
@@ -5,6 +5,6 @@ do
 ps -C $i
 if [ $? -ne 0 ]
 then
-echo "The service is stopped" | mail -s "The service was stopped" jagadeesh.b324@gmail.com
+echo "The service is stopped" | mail -s "The service was stopped" mcpooja29114@gmail.com
 fi
 done
